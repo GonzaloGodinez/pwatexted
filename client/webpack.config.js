7f5b82c45d1dad99424a0f28fac3e033533e3c98
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2024-01-12 13:51:20 -0700

    editerd webpack entries

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,7 +13,6 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-      cards: './src/js/cards.js'
     },
     // Ouput for all bundles
     output: {
@@ -24,7 +23,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'Text Editor'
       }),
       
       // Injects our custom service worker
@@ -37,9 +36,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!!',
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: 'Another Text Editor!!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
